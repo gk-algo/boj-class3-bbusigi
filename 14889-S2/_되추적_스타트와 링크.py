d39a3e6d35723286a2b825d07f1862e130bf0afd
= <jun2mun@naver,com>
2022-09-10 17:40:19 +0900

    14889번 이준범

---
+++ 14889-S2/_되추적_스타트와 링크.py
@@ -0,0 +1,37 @@
+import sys
+input = sys.stdin.readline
+
+
+def DFS(iter,idx):
+    global result
+
+
+    if iter == (N // 2):
+        linkteam = 0 ; starteam = 0
+        for i in range(N):
+            for j in range(i+1,N):# i+1 0 0 제외
+                if visited[i] and visited[j]:
+                    linkteam += (graph[i][j] + graph[j][i])
+
+                elif not visited[i] and not visited[j]:
+                    starteam += (graph[i][j] + graph[j][i])
+            
+        result = min(result,abs(linkteam-starteam))
+        return
+
+            
+    for i in range(idx,N):
+        if not visited[i]:
+            visited[i] = True # 방문
+            DFS(iter+1,i+1) # 방문 처리 후 (나머지 애들 백트래킹) || 재귀
+            visited[i] = False # 방문처리 빼기
+
+
+if __name__ == '__main__':
+
+    result = sys.maxsize
+    N = int(input()) # 축구를 하기 위해 모인 사람 (4<=N<=20) N은 짝수
+    visited = [0 for _ in range(N)] # 방문여부
+    graph = [list(map(int,input().split())) for _ in range(N)] # 능력치 행렬
+    DFS(0,0)
+    print(result)
